dddd59969479497d6e5d2a250917902615ba7b74
sauravjaiswalsj <sauravjaiswal999@gmail.com>
2020-09-25 21:01:12 +0530

    Computing distance between two points

---
+++ DukeUni/Revision/DistanceBetweenPoint.java
@@ -0,0 +1,25 @@
+public class DistanceBetweenPoint {
+    private final int x;
+    private final int y;
+    public DistanceBetweenPoint(int startX, int startY){
+        this.x=startX;
+        this.y=startY;
+    }
+    public int getX(){
+        return this.x;
+    }
+    public int getY(){
+        return this.y;
+    }
+    //compute Distance from one point to another point
+    public double distance(DistanceBetweenPoint otherPt){
+        int dx = x - otherPt.getX();
+        int dy = y - otherPt.getY();
+        return  Math.sqrt((dx*dx + dy*dy));
+    }
+    public static void main(String[] args){
+        DistanceBetweenPoint p1 = new DistanceBetweenPoint(3,4);
+        DistanceBetweenPoint p2 = new DistanceBetweenPoint(6,8);
+        System.out.println(p1.distance(p2));
+    }
+}
