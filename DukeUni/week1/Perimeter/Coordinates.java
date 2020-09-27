a47e3f1c299baa0e8e44a92babe8833452ba2582
sauravjaiswalsj <sauravjaiswal999@gmail.com>
2020-09-27 21:42:55 +0530

    adding Perimeter Assignment

--- DukeUni/week1/Perimeter/Coordinates.java
+++ DukeUni/week1/Perimeter/Coordinates.java
@@ -8,15 +8,21 @@ public class Coordinates {
         this.x = pointX;
         this.y=pointY;
     }
+
+    public Coordinates() {
+        this.x=0;
+        this.y=0;
+    }
+
     public int getX(){
         return this.x;
     }
     public int getY(){
         return this.y;
     }
-    public double getDistance(Coordinates C){
-        int dx=C.getX();
-        int dy=C.getY();
+    public double getDistance(Coordinates C,Coordinates D){
+        int dx=C.getX()-D.getX();
+        int dy=C.getY()-D.getY();
         return Math.sqrt((dx*dx + dy*dy));
     }
 }
