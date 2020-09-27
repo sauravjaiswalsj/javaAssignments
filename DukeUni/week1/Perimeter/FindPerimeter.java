b7085cd19ba9de2f9aaebb892fe8724dcc50917d
sauravjaiswalsj <sauravjaiswal999@gmail.com>
2020-09-27 21:43:28 +0530

    Adding Final Perimeter Method

--- DukeUni/week1/Perimeter/FindPerimeter.java
+++ DukeUni/week1/Perimeter/FindPerimeter.java
@@ -7,6 +7,14 @@ public class FindPerimeter {
            System.out.println(getPerimeter(new Shape()));
     }
     public static double getPerimeter(Shape s){
-        
+        Coordinates c= new Coordinates();
+        double totalPerim=0.0;
+        Coordinates lastCord=s.getLastPoint();
+        for(Coordinates currCord:s.getPoints()){
+            double currDist=c.getDistance(lastCord,currCord);
+            totalPerim+=currDist;
+            lastCord=currCord;
+        }
+        return totalPerim;
     }
 }
