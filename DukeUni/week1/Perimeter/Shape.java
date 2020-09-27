034992b72ba0a9af9c2b346e0a98709da43632a9
sauravjaiswalsj <sauravjaiswal999@gmail.com>
2020-09-27 20:04:48 +0530

    Adding Shape

--- DukeUni/week1/Perimeter/Shape.java
+++ DukeUni/week1/Perimeter/Shape.java
@@ -1,6 +1,4 @@
 package Perimeter;
-import Perimeter.ReadFile;
-import Perimeter.Coordinates;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -9,6 +7,22 @@ public class Shape {
     private ArrayList<Coordinates> points;
     public void parseStringToNum() throws IOException {
         String line = new ReadFile().getReadFile();
-        System.out.println(line);
+       // System.out.println(line);
+        String[] parser = line.split("\\s|,| ");
+        points = new ArrayList<>();
+        for(int i=0;i<parser.length-1;i+=2){
+            int x=Integer.parseInt(parser[i]);
+            int y=Integer.parseInt(parser[i+1]);
+            points.add(new Coordinates(x,y));
+        }
+    }
+    public Shape() throws IOException {
+        parseStringToNum();
+    }
+    public Coordinates getLastPoint(){
+        return points.get(points.size()-1);
+    }
+    public ArrayList<Coordinates> getPoints(){
+        return points;
     }
 }
