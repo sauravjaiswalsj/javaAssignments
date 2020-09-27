8d1b13c48b4799f0f97cac9df0c9d8760f10850c
sauravjaiswalsj <sauravjaiswal999@gmail.com>
2020-09-27 17:37:23 +0530

    added Parse Method

--- DukeUni/week1/Perimeter/Shape.java
+++ DukeUni/week1/Perimeter/Shape.java
@@ -1,4 +1,14 @@
 package Perimeter;
+import Perimeter.ReadFile;
+import Perimeter.Coordinates;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Shape {
+    private ArrayList<Coordinates> points;
+    public void parseStringToNum() throws IOException {
+        String line = new ReadFile().getReadFile();
+        System.out.println(line);
+    }
 }
