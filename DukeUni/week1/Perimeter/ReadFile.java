53e4239d7c8f6e7669dce796b6aa43ca7d3c9dac
sauravjaiswalsj <sauravjaiswal999@gmail.com>
2020-09-27 17:20:57 +0530

    adding Read File

---
+++ DukeUni/week1/Perimeter/ReadFile.java
@@ -0,0 +1,23 @@
+package Perimeter;
+import java.io.*;
+
+public class ReadFile{
+    public ReadFile() throws IOException {
+        StringBuilder sb = new StringBuilder(new BufferedReader(new InputStreamReader(System.in)).readLine());
+        if(sb.indexOf(".txt")==-1){
+            sb.append(".txt");
+        }
+        try {
+            final File file = new File(sb.toString());
+            final BufferedReader br = new BufferedReader(new FileReader(file));
+            sb=new StringBuilder("");
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line).append(" ");
+            }
+            System.out.println(sb.toString());
+        }catch (Exception e){
+            System.out.println("404! File Not Found");
+        }
+    }
+}
