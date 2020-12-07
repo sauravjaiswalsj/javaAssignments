ba29eaf9f71ae3602566ba090f6f6a8fc90cf1e8
sauravjaiswalsj <sauravjaiswal999@gmail.com>
2020-12-08 00:09:02 +0530

    adding GeneFinding Algo

---
+++ DukeUni/Revision/GeneFinding.java
@@ -0,0 +1,16 @@
+import java.util.*;
+/**
+ * ATCG
+ * Codon -"ATC" 3 nucleotide
+ * Nucleotide - A or T or C or G
+ * Start Codon - ATG
+ * Stop codon - TAA
+ */
+/*
+    Finding Gene
+    Find 1st occurrence of Start and stop codon
+
+ */
+public class GeneFinding {
+
+}
