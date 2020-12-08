b97c8a9c2529a2299f97101a6870f759982ac79e
sauravjaiswalsj <sauravjaiswal999@gmail.com>
2020-12-08 19:42:21 +0530

    adding Finding Genes Problem

--- SolvingProblemWithSoftware/src/Week2/FindingGene.java
+++ SolvingProblemWithSoftware/src/Week2/FindingGene.java
@@ -1,5 +1,38 @@
 package Week2;
+//Read file as a string search for the gene and return the 1st gene
+import java.io.*;
+import java.util.Locale;
+
+class FindGene{
+    public String getGene(String DNA){
+       String startCodon = "ATG";
+       String stopCodon = "TAA";
+       return DNA.substring(DNA.indexOf(startCodon),DNA.indexOf(stopCodon)+3);
+    }
+}
 
 public class FindingGene {
-
+    public static void main(String[] args){
+        String DNA = getDNA();
+        if(!DNA.equals("File Not Found"))
+            System.out.println(new FindGene().getGene(DNA.toUpperCase()));
+        else
+            System.out.println("File Not Found");
+    }
+    public static String getDNA(){
+        try {
+            StringBuilder sb = new StringBuilder(new BufferedReader(new InputStreamReader(System.in)).readLine());
+            if(sb.indexOf(".txt")==-1){
+                sb.append(".txt");
+            }
+            File file = new File("./src/Week2/"+sb.toString());
+            final BufferedReader br = new BufferedReader( new FileReader(file));
+            sb.delete(0,sb.length());
+            sb.append(br.readLine());
+            return sb.toString();
+        }
+        catch (Exception e){
+            return "File Not Found";
+        }
+    }
 }
