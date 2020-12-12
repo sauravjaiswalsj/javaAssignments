be56bb98a035e42cd6fbe4e79d7d8b02b9d343c2
sauravjaiswalsj <sauravjaiswal999@gmail.com>
2020-12-13 01:24:45 +0530

    Two Occurence:refractored

--- SolvingProblemWithSoftware/src/Week2/TwoOccurrence.java
+++ SolvingProblemWithSoftware/src/Week2/TwoOccurrence.java
@@ -1,6 +1,9 @@
 package Week2;
+
+import javax.management.ObjectName;
+
 class Occurrence{
-    public boolean towOccurrences(String checkPresentString, String string){
+    public boolean towOccurrences(String string, String checkPresentString ){
         //returns true if the occurrence occurs at least twice in b else false.
         int currIndex = 0,count=0,prevIndex=-1;
         while(currIndex!=-1) {
@@ -21,9 +24,45 @@ class Occurrence{
         //}
         return count >= 2;
     }
+    public String getLastString(String a, String b){
+        int index = a.indexOf(b);
+
+        if(index==-1) {
+            index = 0;
+        }else {
+            index+=b.length();
+        }
+        return a.substring(index);
+    }
 }
+
 public class TwoOccurrence {
     public static void main(String[] args){
-        System.out.println(new Occurrence().towOccurrences("a","aanana"));
+        Occurrence occurrence =new Occurrence();
+        testMethod1(occurrence);
+        testMethod2(occurrence);
+        testMethod3(occurrence);
+        testMethod4(occurrence);
     }
+    public static void testMethod1(Occurrence occurrence){
+        String a = "banana";
+        String b = "an";
+        System.out.println(occurrence.towOccurrences(a,b)+" "+occurrence.getLastString(a,b));
+    }
+    public static void testMethod2(Occurrence occurrence){
+        String a = "ananana";
+        String b = "a";
+        System.out.println(occurrence.towOccurrences(a,b)+" "+occurrence.getLastString(a,b));
+    }
+    public static void testMethod3(Occurrence occurrence){
+        String a = "ctgtatgta";
+        String b = "atg";
+        System.out.println(occurrence.towOccurrences(a,b)+" "+occurrence.getLastString(a,b));
+    }
+    public static void testMethod4(Occurrence occurrence){
+        String a = "forest";
+        String b = "atg";
+        System.out.println(occurrence.towOccurrences(a,b)+" "+occurrence.getLastString(a,b));
+    }
+
 }
