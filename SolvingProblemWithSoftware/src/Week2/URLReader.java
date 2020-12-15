b6efd28555610df8e7d79d8f70fbbec92e8537e9
sauravjaiswalsj <sauravjaiswal999@gmail.com>
2020-12-15 09:12:51 +0530

    URL Reader

--- SolvingProblemWithSoftware/src/Week2/URLReader.java
+++ SolvingProblemWithSoftware/src/Week2/URLReader.java
@@ -1,5 +1,20 @@
 package Week2;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class URLReader {
-
+    public static void main(String[] args)throws IOException{
+        final URL url = new URL("http://www.dukelearntoprogram.com/course2/data/manylinks.html");
+        URLConnection conn = url.openConnection();
+        final BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line;
+        StringBuilder sb = new StringBuilder();
+        while((line=br.readLine())!=null){
+            sb.append(line).append("\n");
+        }
+        System.out.println(sb.toString());
+    }
 }
