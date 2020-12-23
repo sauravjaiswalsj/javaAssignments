64e2c43f845c07f5ccc86724f539ae97189baf2b
sauravjaiswalsj <sauravjaiswal999@gmail.com>
2020-12-23 22:15:12 +0530

    adding URL Readed

--- SolvingProblemWithSoftware/src/Week2/URLRead/ReadURL.java
+++ SolvingProblemWithSoftware/src/Week2/URLRead/ReadURL.java
@@ -1,20 +1,20 @@
-package Week2;
+package Week2.URLRead;
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
-import java.net.URLConnection;
-
-public class URLReader {
+public class ReadURL {
     public static void main(String[] args)throws IOException{
         final URL url = new URL("https://www.dukelearntoprogram.com//course2/data/manylinks.html");
-        URLConnection conn = url.openConnection();
-        final BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        //Open a url using java
+        //java.awt.Desktop.getDesktop().browse(java.net.URI.create(String.valueOf(url)));
+        final BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
         String line;
         StringBuilder sb = new StringBuilder();
         while((line=br.readLine())!=null){
             sb.append(line).append("\n");
         }
+        br.close();
         System.out.println(sb.toString());
     }
 }
